e314efc06c4acc894f3c340c5314c86401b06d3d
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-03-21 13:20:01 -0400

    now supports searches

--- upload-csv.py
+++ upload-csv.py
@@ -14,7 +14,7 @@ auth = tweepy.OAuthHandler(os.environ.get('API_KEY'), os.environ.get('API_SECRET
 auth.set_access_token(os.environ.get('ACCESS_TOKEN'), os.environ.get('ACCESS_SECRET'))
 api = tweepy.API(auth)
 
-
+MAX_SEARCH = 1000
 
 bert_model_path = "sentiment140_bert"
 bert_model = tf.saved_model.load(bert_model_path)
@@ -46,7 +46,7 @@ def new_val(prob):
 
 reformat = np.vectorize(new_val)
 
-def getSentiments(usernames):
+def getSentiments(queries,user_query):
     """
     Input: username
     Returns: List of tweets in last 30 days
@@ -56,10 +56,14 @@ def getSentiments(usernames):
     indices = []
     ind = 0
     sentiments = []
-    for username in usernames:
+    for query in queries:
         indices.append(ind)
-        print(username)
-        for status in tweepy.Cursor(api.user_timeline,id=username).items():
+        print(query)
+        if user_query:
+            statuses = tweepy.Cursor(api.user_timeline,id=query).items()
+        else:
+            statuses = tweepy.Cursor(api.search, q=query).items(MAX_SEARCH)
+        for status in statuses:
             if status.created_at > thirty_earlier:
                 tweets.append(status.text)
                 ind += 1
@@ -70,53 +74,57 @@ def getSentiments(usernames):
     preprocessed = preprocess(np.array(tweets))
     print("Making predictions")
     predictions = reformat(tf.sigmoid(bert_model(tf.constant(preprocessed))))
-    for i in range(len(usernames)):
+    for i in range(len(queries)):
         if indices[i+1] == indices[i]:
             sentiments.append(np.nan)
         else:
-            sentiments.append(round(np.mean(predictions[indices[i]:indices[i+1]])*1000))
+            sentiments.append(int(round(np.mean(predictions[indices[i]:indices[i+1]])*1000)))
     return sentiments
 
 if __name__=='__main__':
-    users_needed = []
-    with open('users.txt', 'r') as f:
-        for line in f:
-            users_needed.append(line.strip())
-
     user = os.environ.get('WP_USER')
     password = os.environ.get('WP_PASSWORD')
     credentials = user + ':' + password
     token = base64.b64encode(credentials.encode())
     header = {'Authorization': 'Basic ' + token.decode('utf-8')}
     response = requests.get('https://unionpoll.com/wp-json/wp/v2/media')
-    response = response.json()
-    found = False
-    for media in response:
-        if 'users.csv' in media['source_url']:
-            id = media['id']
-            file_url = media['source_url']
-            found = True
-    assert found, "csv file not found"
-    print("Retrieving file from "+file_url)
-    df = pd.read_csv(file_url)
-    cols = list(df.columns)
-    assert cols[0] == 'Date', 'First column must be Date'
-    for user in users_needed:
-        if user not in cols:
-            df[user] = np.nan
-            cols.append(user)
+    files_total = response.json()
+    files_needed = ['users', 'searches']
+    for file in files_needed:
+        queries_needed = []
+        with open(file+'.txt', 'r') as f:
+            for line in f:
+                queries_needed.append(line.strip())
+        found = False
+        for media in files_total:
+            if file + '.csv' in media['source_url']:
+                id = media['id']
+                file_url = media['source_url']
+                found = True
+                break
+        assert found, "csv file not found"
+        print("Retrieving file from "+file_url)
+        df = pd.read_csv(file_url)
+        cols = list(df.columns)
+        assert cols[0] == 'Date', 'First column must be Date'
+        for query in queries_needed:
+            if query not in cols:
+                print(query, " added to csv")
+                df[query] = np.nan
+                cols.append(query)
 
-    day = datetime.datetime.today().strftime('%Y-%m-%d')
-    if day in df['Date']:
-        print('Already done')
+        day = datetime.datetime.today().strftime('%Y-%m-%d')
+        if day in df['Date'].unique():
+            print('Already done')
 
-    else:
-        print('New day, getting predictions')
-        preds = getSentiments(cols[1:])
-        df.loc[len(df)] = [day] + preds
-        df.to_csv('users.csv', index=False)
-        files = {'file': open('users.csv', 'rb')}
+        else:
+            print('New day, getting predictions')
+            preds = getSentiments(cols[1:],'user' in file)
+            print('Writing to csv and uploading')
+            df.loc[len(df)] = [day] + preds
+            df.to_csv(file+'.csv', index=False)
+            files = {'file': open(file+'.csv', 'rb')}
 
-        response = requests.delete('https://unionpoll.com/wp-json/wp/v2/media/'+str(id)+'/?force=1',headers=header)
+            response = requests.delete('https://unionpoll.com/wp-json/wp/v2/media/'+str(id)+'/?force=1',headers=header)
 
-        response = requests.post('https://unionpoll.com/wp-json/wp/v2/media',headers=header,files=files)
+            response = requests.post('https://unionpoll.com/wp-json/wp/v2/media',headers=header,files=files)
